996d3d479543f7851e47bfdc4e5f80a699fd1ea0
root <blsun@redhat.com>
2014-09-10 15:27:48 +0800

    panic module

---
+++ module/panic/hello.c
@@ -0,0 +1,17 @@
+#include <linux/module.h>
+#include <linux/init.h>
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("SBL");
+MODULE_DESCRIPTION("panic");
+static int __init hello_init(void)
+{
+    printk(KERN_ERR "hello world!\n");
+	panic("haha!");
+    return 0;
+}
+static void __exit hello_exit(void)
+{
+    printk(KERN_EMERG "hello exit!\n");
+}
+module_init(hello_init);
+module_exit(hello_exit);
